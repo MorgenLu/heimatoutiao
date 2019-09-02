45dd17f7d57054d64a6c4e7de627b5e5ce76e178
卢一斌 <850378029@qq.com>
2019-09-02 11:02:53 +0800

    黑马头条PC-主页模块-头部退出系统

--- src/components/home/layout-header.vue
+++ src/components/home/layout-header.vue
@@ -6,15 +6,15 @@
     </el-col>
     <el-col :span="3" class="right">
       <img :src="userInfo.photo ? userInfo.photo : defaultImg" alt />
-      <el-dropdown trigger="click">
+      <el-dropdown trigger="click" @command="handleMenuItem">
         <span class="el-dropdown-link">
           {{userInfo.name}}
           <i class="el-icon-arrow-down el-icon--right"></i>
         </span>
         <el-dropdown-menu slot="dropdown">
-          <el-dropdown-item>个人信息</el-dropdown-item>
-          <el-dropdown-item>git地址</el-dropdown-item>
-          <el-dropdown-item>退出</el-dropdown-item>
+          <el-dropdown-item command="account">个人信息</el-dropdown-item>
+          <el-dropdown-item command="git">git地址</el-dropdown-item>
+          <el-dropdown-item command="lgout">退出</el-dropdown-item>
         </el-dropdown-menu>
       </el-dropdown>
     </el-col>
@@ -44,6 +44,16 @@ export default {
         .catch(err => {
           console.log(err)
         })
+    },
+    handleMenuItem (command) {
+      if (command === 'account') {
+      } else if (command === 'git') {
+        window.location.href =
+          'https://github.com/MorgenLu/heimatoutiao/commits/master'
+      } else {
+        window.localStorage.clear()
+        this.$router.push('/login')
+      }
     }
   },
   created () {
