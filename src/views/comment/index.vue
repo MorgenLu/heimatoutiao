9cd51627f35cb15bc96fda30fdb8ae153b5aeba7
卢一斌 <850378029@qq.com>
2019-09-03 21:46:21 +0800

    黑马头条PC-评论列表-评论数据加载到页面及状态过滤

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -3,12 +3,17 @@
     <bread-crumb slot="header">
       <template slot="title">评论列表</template>
     </bread-crumb>
-    <el-table :data="data" style="width: 100%">
-      <el-table-column prop="date" label="标题" width="700px"></el-table-column>
-      <el-table-column prop="name" label="评论状态"></el-table-column>
-      <el-table-column prop="address" label="总评论数"></el-table-column>
-      <el-table-column prop="name" label="粉丝评论数"></el-table-column>
-      <el-table-column prop="address" label="操作"></el-table-column>
+    <el-table :data="list" style="width: 100%">
+      <el-table-column prop="title" label="标题" width="700px"></el-table-column>
+      <el-table-column :formatter="formatter" prop="comment_status" label="评论状态"></el-table-column>
+      <el-table-column prop="total_comment_count" label="总评论数"></el-table-column>
+      <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="obj">
+          <el-button type="text">修改评论</el-button>
+          <el-button type="text">{{obj.row.comment_status ? '关闭评论':'开启评论'}}</el-button>
+        </template>
+      </el-table-column>
     </el-table>
   </el-card>
 </template>
@@ -16,7 +21,36 @@
 <script>
 export default {
   data () {
-    return {}
+    return {
+      list: [],
+      page: {
+        page: 1, // 当前页码
+        pageSize: 10, // 当前每页条数
+        total: 0 // 总条数
+      }
+    }
+  },
+
+  methods: {
+    formatter (row) {
+      return row.comment_status ? '正常' : '关闭'
+    },
+    getComments () {
+      this.$axios({
+        url: '/articles',
+        params: {
+          response_type: 'comment',
+          page: this.page.page,
+          per_page: this.page.pageSize
+        }
+      }).then(res => {
+        this.list = res.data.results
+        // this.page.total = result.data.total_count;
+      })
+    }
+  },
+  created () {
+    this.getComments()
   }
 }
 </script>
