9ac28dc12cb3687566d6aa7b3fedf666767be55a
卢一斌 <850378029@qq.com>
2019-09-03 20:25:08 +0800

    黑马头条PC-评论列表-页面结构-table的使用

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,10 +1,28 @@
 <template>
-  <div>评论列表</div>
+  <el-card class="box-card">
+    <bread-crumb slot="header">
+      <template slot="title">评论列表</template>
+    </bread-crumb>
+    <el-table :data="data" style="width: 100%">
+      <el-table-column prop="date" label="标题" width="700px"></el-table-column>
+      <el-table-column prop="name" label="评论状态"></el-table-column>
+      <el-table-column prop="address" label="总评论数"></el-table-column>
+      <el-table-column prop="name" label="粉丝评论数"></el-table-column>
+      <el-table-column prop="address" label="操作"></el-table-column>
+    </el-table>
+  </el-card>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {}
+  }
+}
 </script>
 
 <style>
+.box-card {
+  margin-top: 20px;
+}
 </style>
