1737a8e6d9377ac3dd72f61d78deeb83ab6ad6a3
卢一斌 <850378029@qq.com>
2019-09-03 19:06:54 +0800

    黑马头条PC-面包屑组件封装

--- src/main.js
+++ src/main.js
@@ -4,11 +4,12 @@ import router from './permission'
 import ElementUI from 'element-ui'
 import axios from './utils/axios.config'
 import Component from './components'
+
 import 'element-ui/lib/theme-chalk/index.css'
 import './style/index.less'
 Vue.use(ElementUI)
 Vue.use(Component)
-axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 
