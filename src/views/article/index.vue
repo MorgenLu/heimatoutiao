7ca9c56c040ba5a5d719b257c840c6d10246f0d7
卢一斌 <850378029@qq.com>
2019-09-05 11:46:31 +0800

    黑马头条PC-内容列表-页面结构-搜索筛选

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -6,45 +6,52 @@
     <!-- 搜索工具栏 -->
     <el-form>
       <el-form-item label="文章状态">
-        <el-radio-group>
-          <el-radio>全部</el-radio>
-          <el-radio>草稿</el-radio>
-          <el-radio>待审核</el-radio>
-          <el-radio>审核通过</el-radio>
-          <el-radio>审核失败</el-radio>
+        <el-radio-group @change="changeCondition" v-model="searchForm.status">
+          <el-radio :label="5">全部</el-radio>
+          <el-radio :label="0">草稿</el-radio>
+          <el-radio :label="1">待审核</el-radio>
+          <el-radio :label="2">审核通过</el-radio>
+          <el-radio :label="3">审核失败</el-radio>
         </el-radio-group>
       </el-form-item>
       <el-form-item label="频道列表">
-        <el-select></el-select>
+        <el-select @change="changeCondition" v-model="searchForm.channel_id">
+          <el-option v-for="item in clannels" :key="item.id" :label="item.name" :value="item.id"></el-option>
+        </el-select>
       </el-form-item>
       <el-form-item label="时间选择">
         <div class="block">
           <el-date-picker
+            @change="changeCondition"
             type="daterange"
-            range-separator="至"
             start-placeholder="开始日期"
             end-placeholder="结束日期"
+            v-model="searchForm.dateRange"
+            value-format="yyyy-MM-dd"
           ></el-date-picker>
         </div>
       </el-form-item>
     </el-form>
+    {{searchForm}}
     <div>共找到x条符合条件的内容</div>
-    <div class="article_center">
-      <div class="left">
-        <img src="../../assets/img/404.png" alt />
-        <div class="left_center">
-          <span>标题</span>
-          <el-tag type="success">标签二</el-tag>
-          <span>2019-09-04 22:50:51</span>
+    <div class="article-list">
+      <div class="article_center" v-for="(item,index) in list" :key="index">
+        <div class="left">
+          <img :src="item.cover.images.length ? item.cover.images[0] : defaultImg" alt />
+          <div class="left_center">
+            <span>{{item.title}}</span>
+            <el-tag type="success">{{item.status | statusText}}</el-tag>
+            <span>{{item.pubdate}}</span>
+          </div>
         </div>
-      </div>
-      <div class="right">
-        <span>
-          <i class="el-icon-edit"></i>修改
-        </span>
-        <span>
-          <i class="el-icon-edit"></i>删除
-        </span>
+        <div class="right">
+          <span>
+            <i class="el-icon-edit"></i>修改
+          </span>
+          <span>
+            <i class="el-icon-edit"></i>删除
+          </span>
+        </div>
       </div>
     </div>
   </el-card>
@@ -54,20 +61,72 @@
 export default {
   data () {
     return {
-      list: []
+      list: [],
+      defaultImg: require('../../assets/img/404.png'),
+      searchForm: {
+        status: 5,
+        channel_id: null,
+        dateRange: []
+      },
+      clannels: []
     }
   },
   methods: {
-    getArticles () {
+    getclannels () {
       this.$axios({
-        url: '/articles'
+        url: '/channels'
       }).then(res => {
-        console.log(res)
+        this.clannels = res.data.channels
+      })
+    },
+    // 改变搜索条件时
+    changeCondition () {
+      this.getConditionArticle()
+    },
+    // 根据条件查询数据
+    getConditionArticle () {
+      let params = {
+        status: this.searchForm.status === 5 ? null : this.searchForm.status,
+        channel_id: this.searchForm.channel_id,
+        begin_pubdate:
+          this.searchForm.dateRange.length > 0
+            ? this.searchForm.dateRange[0]
+            : null,
+        end_pubdate:
+          this.searchForm.dateRange.length > 1
+            ? this.searchForm.dateRange[1]
+            : null
+      }
+      this.getArticles(params)
+    },
+    getArticles (params) {
+      this.$axios({
+        url: '/articles',
+        params
+      }).then(res => {
+        this.list = res.data.results
       })
     }
   },
   created () {
     this.getArticles()
+    this.getclannels()
+  },
+  filters: {
+    statusText: function (value) {
+      switch (value) {
+        case 0:
+          return '草稿'
+        case 1:
+          return '待审核'
+        case 2:
+          return '已发表'
+        case 3:
+          return '审核失败'
+        default:
+          break
+      }
+    }
   }
 }
 </script>
