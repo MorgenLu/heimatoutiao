2613b0070a74291a236f802b511ed8a6c81edc24
卢一斌 <850378029@qq.com>
2019-09-06 13:29:51 +0800

    黑马头条PC-发表文章-页面结构-简单实现-频道数据加载

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -1,10 +1,66 @@
 <template>
-  <div>发布</div>
+  <el-card class="publish">
+    <bread-crumb slot="header">
+      <template slot="title">发表文章</template>
+    </bread-crumb>
+    <el-form label-width="80px" class="publish_form">
+      <el-form-item label="标题">
+        <el-input style="width:600px"></el-input>
+      </el-form-item>
+      <el-form-item label="内容">
+        <el-input type="textarea" style="width:800px" :rows="10"></el-input>
+      </el-form-item>
+      <el-form-item label="封面">
+        <el-radio-group>
+          <el-radio label="单图"></el-radio>
+          <el-radio label="三图"></el-radio>
+          <el-radio label="无图"></el-radio>
+          <el-radio label="自动"></el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="频道">
+        <el-select v-model="formData.channel_id">
+          <el-option :label="item.name" :value="item.id" v-for="item in channels" :key="item.id"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary">发表</el-button>
+        <el-button>存入草稿</el-button>
+      </el-form-item>
+    </el-form>
+  </el-card>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      channels: [],
+      formData: {
+        channel_id: null
+      }
+    }
+  },
+  methods: {
+    getPublishChannel () {
+      this.$axios({
+        url: '/channels'
+      }).then(res => {
+        console.log(res)
+        this.channels = res.data.channels
+      })
+    }
+  },
+  created () {
+    this.getPublishChannel()
+  }
+}
 </script>
 
-<style>
+<style lang="less" scoped>
+.publish {
+  .publish_form {
+    margin: 50px 0 0 200px;
+  }
+}
 </style>
